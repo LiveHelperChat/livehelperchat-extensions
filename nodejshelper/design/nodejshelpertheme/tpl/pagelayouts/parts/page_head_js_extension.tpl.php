4f07547f9d5c52deb432cafea2b52148c9db1494
Remdex <remdex@gmail.com>
2014-01-19 22:05:02 +0100

    Connect to node only if required

--- nodejshelper/design/nodejshelpertheme/tpl/pagelayouts/parts/page_head_js_extension.tpl.php
+++ nodejshelper/design/nodejshelpertheme/tpl/pagelayouts/parts/page_head_js_extension.tpl.php
@@ -4,9 +4,10 @@
 		'host' => 'localhost',
 		'port' => '31129',
 	);
-?>
+if ( (isset($Result['is_sync_required']) && $Result['is_sync_required'] === true) || erLhcoreClassSystem::instance()->SiteAccess == 'site_admin' ) : ?>
 <script>
 var nodejshelperHostConnect = '<?php echo $nodeJsHelperSettings['host']?>:<?php echo $nodeJsHelperSettings['port']?>';
 </script>
 <script src="<?php echo $nodeJsHelperSettings['prefix'],$nodeJsHelperSettings['host']?>:<?php echo $nodeJsHelperSettings['port']?>/socket.io/socket.io.js"></script>
 <script type="text/javascript" language="javascript" src="<?php echo erLhcoreClassDesign::designJS('js/customjs.js');?>"></script>
+<?php endif; ?>
