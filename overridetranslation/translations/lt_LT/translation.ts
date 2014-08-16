e6e84ade8eebf67f9c20caa080d73296a083999d
Remdex <remdex@gmail.com>
2014-08-16 08:12:21 +0200

    Override translations example

---
+++ overridetranslation/translations/lt_LT/translation.ts
@@ -0,0 +1,16 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="lt">
+  <context>
+    <name>pagelayout/pagelayout</name>    
+    <message>
+      <source>Extra modules</source>
+      <translation>Override extra modules title in lithuanian language</translation>
+    </message>
+  </context>
+  <context>
+    <name>extensioncontent/extensioncontent</name>    
+    <message>
+      <source>Only in extesnion found text</source>
+      <translation>Tik exteksione randamas tekstas</translation>
+    </message>
+  </context>
+</TS>
