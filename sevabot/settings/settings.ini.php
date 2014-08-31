063c30fc5a01e3d7e519f834621caf2981d79570
Remdex <remdex@gmail.com>
2014-08-31 12:55:31 +0200

    Update default settings

--- sevabot/settings/settings.ini.php
+++ sevabot/settings/settings.ini.php
@@ -3,15 +3,15 @@
 return array(
 	// These users receives all notifications
 	'chat' => array (
-				array ( // You can provide more than one recipient
+				/*array ( // You can provide more than one recipient
 					'chat_id' => '681ee798fdc73f5b384f06db875fd599', // Sevabot chat ID
 					'email' => 'remdex@gmail.com'					 // LHC user e-mail
-				)
+				)*/
 	), 
 		
 	// These users receives notifications by department id
 	'chat_dep' => array (
-				/* 4 => array (
+				/* 4 => array ( // Department ID
 						array (
 							'chat_id' => '681ee798fdc73f5b384f06db875fd599', // Sevabot chat ID
 							'email' => 'remdex@gmail.com'					 // LHC user e-mail
@@ -27,10 +27,10 @@ Click to accept a chat
 {url_accept}", 
 		
 	// Sevabot secret word
-	'sevabot_secret' => 'lhcsevabotdone',
+	'sevabot_secret' => '',
 		
-	// Where sevabot is installed
-	'url' => 'http://146.185.191.107:5000/msg/'
+	// Where sevabot is installed, E.g http://127.0.0.1:5000/msg/
+	'url' => 'http://127.0.0.1:5000/msg/'
 		
 );
 
