371826463b9fa9b34e47f132a74315135aea6c9b
Remdex <remdex@gmail.com>
2015-01-12 19:37:14 +0100

    Enjoy :)

--- unbrand/design/unbrandtheme/tpl/pagelayouts/parts/copyright_meta.tpl.php
+++ unbrand/design/unbrandtheme/tpl/pagelayouts/parts/copyright_meta.tpl.php
@@ -1,6 +1,7 @@
 <?php 
 /**
- * You cannot remove this tag, unless you have made a donation.
+ * Since 2.19v you can remove this even on commercial projects etc.
+ * Read more this article why it was done
  * http://livehelperchat.com/license-term-of-use-removing-links-129a.html
  * 
  * */?>
