1b9e66a4ec5963f3c641ba5eed44af59b31ece37
Remdex <remdex@gmail.com>
2015-01-12 19:42:35 +0100

    Correct link

--- unbrand/design/unbrandtheme/tpl/pagelayouts/parts/copyright_meta.tpl.php
+++ unbrand/design/unbrandtheme/tpl/pagelayouts/parts/copyright_meta.tpl.php
@@ -2,7 +2,7 @@
 /**
  * Since 2.19v you can remove this even on commercial projects etc.
  * Read more this article why it was done
- * http://livehelperchat.com/license-term-of-use-removing-links-129a.html
+ * https://livehelperchat.com/commercial-support-13c.html
  * 
  * */?>
 <meta name="copyright" content="Remigijus Kiminas, livehelperchat.com">
