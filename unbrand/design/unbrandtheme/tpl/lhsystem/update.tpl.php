8460268940050cd082beed68eb48cbcb4535e7e6
Remgijus Kiminas <remdex@gmail.com>
2015-11-28 16:01:32 -0500

    Update to new version

--- unbrand/design/unbrandtheme/tpl/lhsystem/update.tpl.php
+++ unbrand/design/unbrandtheme/tpl/lhsystem/update.tpl.php
@@ -1,15 +1,47 @@
-<h1><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/timezone','Live Support update');?></h1>
+<h1><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/timezone','Live Support Update');?></h1>
 
 <?php if (isset($updated) && $updated == 'done') : $msg = erTranslationClassLhTranslation::getInstance()->getTranslation('system/smtp','Settings updated'); ?>
 	<?php include(erLhcoreClassDesign::designtpl('lhkernel/alert_success.tpl.php'));?>
 <?php endif; ?>
 
 <div class="row">
-	<div class="columns small-6">
+	<div class="col-md-4">
 		<h3><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/update','Your version')?> - <?php echo erLhcoreClassUpdate::LHC_RELEASE/100;?></h3>
-		<h4><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/update','Last database update')?> - update_<?php echo erLhcoreClassUpdate::DB_VERSION?>.sql</h4>		
+		<h3><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/update','Current version')?> - <span class="success-color" id="recent-version">...</span></h3>
+		<a class="btn btn-default btn-xs" href="http://livehelperchat.com/news-5c.html" target="_blank"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/update','News')?></a>
+		<a class="btn btn-default btn-xs" href="http://livehelperchat.com/article/view/63" target="_blank"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/update','Update instructions')?></a>
+		
 	</div>
-	<div class="columns small-6">
-		<div id="database-status"></div>
+	<div class="col-md-8">
+		<div id="status-db"><?php echo erTranslationClassLhTranslation::getInstance()->getTranslation('system/timezone','Comparing current database structure, please wait...')?></div>
 	</div>
 </div>
+
+
+<script>
+function updateDatabase() {
+	$('#db-status-checked').hide();
+	$('#db-status-updating').show();		
+	$.postJSON('<?php echo erLhcoreClassDesign::baseurl('system/update')?>/(action)/statusdbdoupdate',function(data){
+        $('#status-db').html(data.result);            
+    }); 
+};
+
+(function() {
+	
+  $.ajax({
+      url: 'https://livehelperchat.com/update/version',   
+      dataType: 'jsonp',      
+      jsonp: 'callback',
+      jsonpCallback: 'jsonpCallbackLHC',
+      success: function(data){        
+              $('#recent-version').text(data.version/100); 
+      }
+  });
+	 
+  $.postJSON('<?php echo erLhcoreClassDesign::baseurl('system/update')?>/(action)/statusdb',function(data){
+      $('#status-db').html(data.result);            
+  });
+    
+})();
+</script>
