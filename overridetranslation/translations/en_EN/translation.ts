19118e926f6b5809a26ede33097fd84309e24b39
Remigijus Kiminas <remdex@gmail.com>
2020-12-03 00:48:16 -0500

    Override english translation

---
+++ overridetranslation/translations/en_EN/translation.ts
@@ -0,0 +1,9 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="en">
+  <context>
+    <name>chat/startchat</name>
+    <message>
+      <source>Your question</source>
+      <translation>Your super duper question</translation>
+    </message>
+  </context>
+</TS>
